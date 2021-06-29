581d824b386e6603ee543d3b1605e5374bfbe311
Shirin Hk <shirinhk@Shirins-MacBook-Pro.local>
2021-06-29 14:08:49 -0400

    update file

--- js/ddapp.js
+++ js/ddapp.js
@@ -1,14 +1,24 @@
 	// put variables (connections to the web page / DOM) at the top
 	// constant -> something that will never change / can't be redefined
+
+	//identify the nodes of interest in the DOM
 	const puzzleSelectors = document.querySelectorAll("#buttonHolder img"),
 				dropZoneContainer = document.querySelector(".puzzle-board"),
 				dragImages = document.querySelectorAll(".puzzle-image"),
 				dropZones = document.querySelectorAll(".drop-zone");
 
 	// functions go in the middle
-
 	function swapImages() {
 		//swap out the draggable thumbnail dragImages
+		//Update the bckground image of the drop zone dropcontainer
+		//1. get the imageref attribute from the element we're clicking on
+
+		let imageIndex = this.dataset.imageref;
+				//newImagePath = "images/add/backGround" + imageIndex;
+				newImagePath = `../images/add/backGround${newIndex}.jpg`;
+
+				//set the backgroung image of the dropZoneContainer
+				dropcontainer.style.backgroundImage = `url(${newImagePath})`;
 		debugger;
 	}
 
